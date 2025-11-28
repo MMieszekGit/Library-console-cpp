8b0d8b6638b5e239e3d19e093ad5f720cd757fc7
MMieszekGit <151734899+MMieszekGit@users.noreply.github.com>
2025-11-28 07:52:11 +0100

    Update LibraryService.h

--- services/LibraryService.h
+++ services/LibraryService.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "../interfaces/IBookRepo.h"
 #include "../interfaces/IUserRepo.h"
 #include "../interfaces/ILoanRepo.h"
@@ -14,14 +15,14 @@ class LibraryService {
 	ILoanRepo* loanRepo;
 public:
 	LibraryService(IBookRepo& bookRepo, IUserRepo& userRepo, ILoanRepo& loanRepo);
-	Library::AddResult addBook(int bookId, std::string& bookTitle, std::string& bookAuthor, int bookYear, int bookCopies);
+	Library::AddResult addBook(const int bookId, const std::string& bookTitle, const std::string& bookAuthor, const int bookYear, const int bookCopies);
 	std::vector<Book> listAllBooks();
 	std::vector<Book> findMatchingBooks(const std::string& bookTitle, const std::string& bookAuthor);
-	Library::AddResult addUser(int id, std::string& name, std::string& mail);
-	Library::RemoveUserResult removeUser(const std::string userName);
+	Library::AddResult addUser(const int id, std::string& name, std::string& mail);
+	Library::RemoveUserResult removeUser(const std::string& userName);
 	Library::RemoveLoanResult removeLoan(const int loanId);
 	std::vector<User> listAllUsers();
-	Library::AddLoanResult addLoan(const int loanId, const int userId, const int bookId, std::chrono::year_month_day loanDate, std::chrono::year_month_day loanDueDate);
+	Library::AddLoanResult addLoan(const int loanId, const int userId, const int bookId, const std::chrono::year_month_day loanDate, const std::chrono::year_month_day loanDueDate);
 	std::vector<Loan> listUserLoans(const int userId);
 	Library::RenewLoanResult renewLoan(const int loanId);
 };
